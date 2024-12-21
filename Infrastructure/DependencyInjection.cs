e6ce93474ac226f1b0d766fb21dd87df2e231fc4
Admin <admin@gmail.com>
2024-12-21 08:00:12 +0330

    Add ef core to infrastructure layer

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Infrastructure.DataContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            services
+                .AddDataContext(configuration);
+
+            return services;
+        }
+
+        public static IServiceCollection AddDataContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
             return services;
         }
     }
