457425b85c5d68d4ccfbc136af2b57cfd054d125
m3hdi <mehdi.k1917@gmail.com>
2025-02-26 18:15:57 +0330

    Add Swagger JWT Authorization option

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,12 +1,37 @@
-using Application;
+﻿using Application;
 using Infrastructure;
+using Microsoft.OpenApi.Models;
 using Presentation;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter JWT token to log in"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+            },
+            new string[] {}
+        }
+    });
+});
 
 builder.Services
     .AddApplication()
